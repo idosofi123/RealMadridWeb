63171bb3c1fa6a21fac4b699d529ceae167bc61e
Ido Michael Sofi <pikagms@gmail.com>
2021-05-26 18:47:37 +0300

    init(Created the model class for the Stadium entity)

---
+++ RealMadridWebApp/RealMadridWebApp/Models/Stadium.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RealMadridWebApp.Models {
+
+    public class Stadium {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Capacity { get; set; }
+    }
+}
