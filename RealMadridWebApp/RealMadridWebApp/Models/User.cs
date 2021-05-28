9c834aed6ad5613db420a2d3ee8140e16db538dc
talhillel1 <talhillel159@gmail.com>
2021-05-28 16:44:05 +0300

    feat(Create user model)

---
+++ RealMadridWebApp/RealMadridWebApp/Models/User.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RealMadridWebApp.Models
+{
+    public enum UserType
+    {
+        Client,
+        Admin
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public UserType Type { get; set; } = UserType.Client;
+    }
+}
