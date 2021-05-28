db294d33be70460ae633455f944bb5af4a12dd6f
Ido Michael Sofi <pikagms@gmail.com>
2021-05-28 17:45:17 +0300

    feat(Stadium): Added validation to the capacity field

---
+++ RealMadridWebApp/RealMadridWebApp/Migrations/20210528144407_StadiumCapacityPositive.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RealMadridWebApp.Migrations
+{
+    public partial class StadiumCapacityPositive : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- RealMadridWebApp/RealMadridWebApp/Models/Stadium.cs
+++ RealMadridWebApp/RealMadridWebApp/Models/Stadium.cs
@@ -13,8 +13,11 @@ namespace RealMadridWebApp.Models {
         [Required]
         public string Name { get; set; }
 
+        [Required]
+        [Range(0, int.MaxValue, ErrorMessage = "Negative values are not allowed.")]
         public int Capacity { get; set; }
 
+        [Display(Name = "Image Path")]
         public string ImagePath { get; set; }
     }
 }
