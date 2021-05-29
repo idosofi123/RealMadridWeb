e39025c334258c656c2e4389f734c66b6c7ed67d
talhillel1 <talhillel159@gmail.com>
2021-05-29 14:48:17 +0300

    feat(Add login screen)

--- RealMadridWebApp/RealMadridWebApp/Controllers/UsersController.cs
+++ RealMadridWebApp/RealMadridWebApp/Controllers/UsersController.cs
@@ -19,6 +19,65 @@ namespace RealMadridWebApp.Controllers
             _context = context;
         }
 
+        // GET: Users/Login
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        // GET: Users/AccessDenied
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+
+        // POST: Users/Login
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login([Bind("Id,Username,Password")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                var q = _context.User.FirstOrDefault(u => u.Username == user.Username && u.Password == user.Password);
+
+                if (q != null) 
+                {
+
+               //     Signin(q);
+
+                    return RedirectToAction(nameof(Index), "Home");
+                }
+                else
+                {
+                    ViewData["Error"] = "Username and/or password are incorrect.";
+                }
+            }
+            return View(user);
+        }
+
+        //private async void Signin(User account)
+        //{
+        //    var claims = new List<Claim>
+        //        {
+        //            new Claim(ClaimTypes.Name, account.Username),
+        //            new Claim(ClaimTypes.Role, account.Type.ToString()),
+        //        };
+
+        //    var claimsIdentity = new ClaimsIdentity(
+        //        claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+        //    var authProperties = new AuthenticationProperties
+        //    {
+        //        //ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(10)
+        //    };
+
+        //    await HttpContext.SignInAsync(
+        //        CookieAuthenticationDefaults.AuthenticationScheme,
+        //        new ClaimsPrincipal(claimsIdentity),
+        //        authProperties);
+        //}
 
         // GET: Users/Register
         public IActionResult Register()
