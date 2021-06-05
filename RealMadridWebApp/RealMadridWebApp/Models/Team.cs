d55ad5132519bec6e738f014d6f64af52f85c908
Ido Michael Sofi <pikagms@gmail.com>
2021-06-05 14:07:35 +0300

    feat(Team): Added the team model, and linked it to the stadium model (1:1)

---
+++ RealMadridWebApp/RealMadridWebApp/Models/Team.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RealMadridWebApp.Models {
+
+    public class Team {
+
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public int StadiumId { get; set; }
+
+        public Stadium Stadium { get; set; }
+
+        [Display(Name = "Image Path")]
+        public string ImagePath { get; set; }
+    }
+}
