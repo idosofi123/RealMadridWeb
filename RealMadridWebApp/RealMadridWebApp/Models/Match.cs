b393b4ffb107683a47e0d64828189d34be029bf3
Ido Michael Sofi <pikagms@gmail.com>
2021-06-05 16:37:09 +0300

    feat(Match): Added the match model, connected it to the teams model (1:N)

---
+++ RealMadridWebApp/RealMadridWebApp/Models/Match.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RealMadridWebApp.Models {
+
+    public class Match {
+
+        public int Id { get; set; }
+
+        [Required]
+        public int TeamId { get; set; }
+
+        public Team Team { get; set; }
+
+        [Required]
+        public bool isAway { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "Negative values are not allowed.")]
+        public int? GoalsHome { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "Negative values are not allowed.")]
+        public int? GoalsAway { get; set; }
+    }
+}
--- RealMadridWebApp/RealMadridWebApp/Models/Team.cs
+++ RealMadridWebApp/RealMadridWebApp/Models/Team.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace RealMadridWebApp.Models {
 
@@ -9,10 +10,13 @@ namespace RealMadridWebApp.Models {
         [Required]
         public string Name { get; set; }
 
+        [Required]
         [Display(Name = "Stadium")]
         public int StadiumId { get; set; }
 
         public Stadium Stadium { get; set; }
+
+        public List<Match> Matches { get; set; }
 
         [Display(Name = "Image Path")]
         public string ImagePath { get; set; }
