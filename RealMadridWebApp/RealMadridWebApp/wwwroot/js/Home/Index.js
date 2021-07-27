e0829f70b2857588ad21271528cbf1fdb995eaae
talhillel1 <talhillel159@gmail.com>
2021-07-27 20:02:14 +0300

    feat(Home): Fix wrong commit

--- RealMadridWebApp/RealMadridWebApp/wwwroot/js/Home/Index.js
+++ RealMadridWebApp/RealMadridWebApp/wwwroot/js/Home/Index.js
@@ -5,7 +5,7 @@
         }).done(function (match) {
             updateNextMatch(match);
 
-            setCounter(match.date)
+            //setCounter(match.date)
         })
 });
 
@@ -20,28 +20,28 @@ function updateNextMatch(match) {
     $("#routeId").attr("href","/Matches/Details/" + match.id);
 }
 
-function setCounter(matchDate) {
+//function setCounter(matchDate) {
 
-    setInterval(function () {
+//    setInterval(function () {
 
-        var now = new Date().getTime();
+//        var now = new Date().getTime();
 
-        var distance = new Date(matchDate).getTime() - now;
+//        var distance = new Date(matchDate).getTime() - now;
 
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+//        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+//        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+//        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-        // Output the result in an element with id="demo"
-        document.getElementById("counter").innerHTML = "The game starts in: " +days + "d:" + hours + "h:"
-            + minutes + "m:" + seconds + "s";
+//        // Output the result in an element with id="demo"
+//        document.getElementById("counter").innerHTML = "The game starts in: " +days + "d:" + hours + "h:"
+//            + minutes + "m:" + seconds + "s";
 
-        // If the count down is over, write some text 
-        if (distance < 0) {
-            clearInterval(x);
-            document.getElementById("counter").innerHTML = "Match start";
-        }
-    }, 1000);
+//        // If the count down is over, write some text 
+//        if (distance < 0) {
+//            clearInterval(x);
+//            document.getElementById("counter").innerHTML = "Match start";
+//        }
+//    }, 1000);
 
-}
+//}
