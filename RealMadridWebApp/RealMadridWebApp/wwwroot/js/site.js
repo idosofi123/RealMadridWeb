70aa3827f3c8f327c86f4992b20ea66b8c2b2e49
Oriiiii Arazi <oriarazi123@gmail.com>
2021-07-28 17:18:19 +0300

    Feat(nav Canvas): adding js to draw real madrid cf in canvas with stroke and gradient

--- RealMadridWebApp/RealMadridWebApp/wwwroot/js/site.js
+++ RealMadridWebApp/RealMadridWebApp/wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+var madridCanvas = document.getElementById("realMadridCanvas");
+var ctx = madridCanvas.getContext("2d");
+ctx.font = "30px Comic Sans MS";
+var gradient = ctx.createLinearGradient(0, 0, madridCanvas.width, 0);
+gradient.addColorStop("0", "blue");
+gradient.addColorStop(".5", "#0e8af5");
+gradient.addColorStop("1", "blue");
+
+// Fill with gradient
+ctx.strokeStyle = gradient;
+ctx.strokeText("Real Madrid CF", 0, madridCanvas.height/1.5);
