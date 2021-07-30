4322afda7c90bcd4d84cdf3c81dd83502652446b
talhillel1 <talhillel159@gmail.com>
2021-07-30 16:54:47 +0300

    feat(General): Add custom birth date validation

---
+++ RealMadridWebApp/RealMadridWebApp/CustomBirthDateValidation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RealMadridWebApp
+{
+    public class CustomBirthDateValidation : ValidationAttribute
+    {
+            public override bool IsValid(object value)
+            {
+                DateTime dateTime = Convert.ToDateTime(value);
+                return dateTime < DateTime.Now;
+            }
+        }
+}
