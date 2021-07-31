0304d2f4a3b7be6048fa3fd1db0cb9eae80dd491
Ido Michael Sofi <pikagms@gmail.com>
2021-07-31 12:33:59 +0300

    fix(General): Got rid of json error

--- RealMadridWebApp/RealMadridWebApp/Startup.cs
+++ RealMadridWebApp/RealMadridWebApp/Startup.cs
@@ -37,9 +37,6 @@ namespace RealMadridWebApp {
                     options.AccessDeniedPath = "/Unauthorized/Index";
                 });
 
-            services.AddControllers().AddJsonOptions(x =>
-                x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve);
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
