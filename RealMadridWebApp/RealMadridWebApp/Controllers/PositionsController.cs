2bdd77d04b35e0e1582171c03dc4e679fa85a38c
Oriiiii Arazi <oriarazi123@gmail.com>
2021-08-02 13:03:11 +0300

    feat(Position): adding controllet & views

---
+++ RealMadridWebApp/RealMadridWebApp/Controllers/PositionsController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RealMadridWebApp.Data;
+using RealMadridWebApp.Models;
+
+namespace RealMadridWebApp.Controllers
+{
+
+    [Authorize(Roles = "Admin,Manager")]
+    public class PositionsController : Controller
+    {
+        private readonly RealMadridWebAppContext _context;
+
+        public PositionsController(RealMadridWebAppContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Positions
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Position.ToListAsync());
+        }
+
+        // GET: Positions/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var position = await _context.Position
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (position == null)
+            {
+                return NotFound();
+            }
+
+            return View(position);
+        }
+
+        // GET: Positions/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Positions/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,ImagePath,PositionName")] Position position)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(position);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(position);
+        }
+
+        // GET: Positions/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var position = await _context.Position.FindAsync(id);
+            if (position == null)
+            {
+                return NotFound();
+            }
+            return View(position);
+        }
+
+        // POST: Positions/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,ImagePath,PositionName")] Position position)
+        {
+            if (id != position.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(position);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PositionExists(position.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(position);
+        }
+
+        // GET: Positions/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var position = await _context.Position
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (position == null)
+            {
+                return NotFound();
+            }
+
+            return View(position);
+        }
+
+        // POST: Positions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var position = await _context.Position.FindAsync(id);
+            _context.Position.Remove(position);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PositionExists(int id)
+        {
+            return _context.Position.Any(e => e.Id == id);
+        }
+    }
+}
